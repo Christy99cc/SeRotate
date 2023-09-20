e5f1613331e9a4504a547f1c3ecbca422b29ef34
Christy99cc <christy99cc@qq.com>
2023-09-20 17:14:22 +0800

    backup

--- train_exps.sh
+++ train_exps.sh
@@ -51,7 +51,7 @@
 sleep 2s
 OMP_NUM_THREADS=0 ./tools/dist_train.sh configs/myexps/rotated_fcos_r50_fpnse48s_1x_dota_le90.py 2
 sleep 2s
-CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=0 python ./tools/test.py configs/myexps/rotated_fcos_r50_fpnse48s_1x_dota_le90.py ./work_dirs/rotated_fcos_r50_fpnse48s_1x_dota_le90/epoch_12.pth --format-only --eval-options submission_dir=work_dirs/Task1_results_rotated_fcos_r50_fpnse48s_1x_dota_le90_0918
+CUDA_VISIBLE_DEVICES=6 OMP_NUM_THREADS=0 python ./tools/test.py configs/myexps/rotated_fcos_r50_fpnse48s_1x_dota_le90.py ./work_dirs/rotated_fcos_r50_fpnse48s_1x_dota_le90/epoch_12.pth --format-only --eval-options submission_dir=work_dirs/Task1_results_rotated_fcos_r50_fpnse48s_1x_dota_le90_0919
 
 #sleep 2s
 #CUDA_VISIBLE_DEVICES=4,5 OMP_NUM_THREADS=0 ./tools/dist_train.sh configs/myexps/rotated_fcos_r50_atrous_1x_dota_le90111.py 2
